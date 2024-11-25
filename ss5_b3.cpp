7a580413827df3b0a932f6fe0e5e2618abcbca9c
Huidinne <quanghuy432006@gmail.com>
2024-11-25 23:53:04 +0700

    Upload btvn Session5_b3

---
+++ ss5_b3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("nhap vao mot so nguyen duong: ");
+	scanf("%d",&n);
+	int sum=0;
+	for(int i=1; i<=n; i++){
+		sum+=i;
+	}
+	printf("tong cua cac so tu 1 den %d la: %d",n,sum);
+}
